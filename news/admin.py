1a331767ce6aa9201724487f30ebabe38ed84104
Agata <amielczarek09@gmail.com>
2021-06-10 18:34:35 +0200

    Start project and Article model

---
+++ news/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Article 
+
+class DisplayDate(admin.ModelAdmin):
+    readonly_fields = ('date', )
+
+admin.site.register(Article, DisplayDate)  
